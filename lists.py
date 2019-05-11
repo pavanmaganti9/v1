4929a8510eca93d0ac4e76c23da82428811247fd
PVPavanMaganti <pavan@markitome.com>
2019-05-11 16:33:42 +0530

    lists in py

--- lists.py
+++ lists.py
@@ -23,3 +23,21 @@ print(new2)
 
 new2.insert(3,99)
 print(new2)
+
+#remove function: By using the remove function we can remove the first occurrence of the item
+new3 = ['c','o','o','k','i','e']
+new3.remove('o')
+print(new3)
+
+#pop function : By using the pop function we can delete the item by index
+new4 = ['c','o','o','k','i','e']
+new4.pop(-1)
+print(new4)
+
+#count
+print(new4.count('o'))
+
+#sort function
+new5 = [99,1,74,2,3,32,4,5]
+new5.sort()
+print(new5)
