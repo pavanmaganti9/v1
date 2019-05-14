875e05e25b117a1d28e75e960457317d04c97925
PVPavanMaganti <pavan@markitome.com>
2019-05-14 06:51:14 +0530

    shallow copy and deep copy

--- deepshallow.py
+++ deepshallow.py
@@ -4,16 +4,19 @@ a = [1, 2, 3]
 b = [4, 5, 6]
 c = [a, b]
 
+#Using normal assignment operatings to copy:
 d = c
 
 print(id(c) == id(d))         # True - d is the same object as c
 print(id(c[0]) == id(d[0]))    # True - d[0] is the same object as c[0]
 
+#Using a shallow copy:
 d = copy.copy(c)
 
 print(id(c) == id(d))          # False - d is now a new object
 print(id(c[0]) == id(d[0]))    # True - d[0] is the same object as c[0]
 
+#Using a deep copy:
 d = copy.deepcopy(c)
 
 print(id(c) == id(d))          # False - d is now a new object
