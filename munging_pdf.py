f30df5b0efe4633d5aeb5014dcbc612465c1e4e8
PVPavanMaganti <pavan@markitome.com>
2019-06-03 08:09:53 +0530

    munging pdf from url and creating html to pdf file and store in a directory.

--- munging_pdf.py
+++ munging_pdf.py
@@ -3,9 +3,9 @@ import pdfkit
 path_wkthmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
 config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
 sv = pd.read_csv('work.csv',index_col=0)
-sv.to_html('test.html')
-PdfFilename='pdfPrintOut.pdf'
-pdfkit.from_file('test.html', PdfFilename, configuration=config)
+sv.to_html('docs/test.html')
+PdfFilename='docs/pdfPrintOut.pdf'
+pdfkit.from_file('docs/test.html', PdfFilename, configuration=config)
 
 #generating from URL
 pdfkit.from_url('https://www.youtube.com/watch?v=Fo7J2dHpMjk','sample1.pdf', configuration=config)
