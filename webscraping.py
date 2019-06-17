6a9e4debc3ff4dc43df94beebae81ff09eea63a5
PVPavanMaganti <pavan@markitome.com>
2019-06-17 08:27:09 +0530

    web scraping

--- webscraping.py
+++ webscraping.py
@@ -4,5 +4,22 @@ from csv import writer
 response = urllib.request.urlopen('https://simpleisbetterthancomplex.com/')
 
 data = response.read()
-print(data)
-#soup = BeautifulSoup(data,'html.parser')
+#print(data)
+soup = BeautifulSoup(data,'html.parser')
+#print(soup.body)
+#print(soup.head)
+#print(soup.head.title.text)
+
+#el = soup.find('div')
+
+#el = soup.find_all('div')
+
+#el = soup.find_all('div')[0]
+
+#el = soup.find(class_="post-title").get_text()
+
+for item in soup.select(".post-title"):
+    print(item.get_text())
+
+el = soup.body.contents
+print(el)
