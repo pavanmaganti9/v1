a042a9b6e8003b8881cacb40f49f214b31508f16
PVPavanMaganti <pavan@markitome.com>
2019-06-30 12:36:29 +0530

    tech m interview question count length of string without using len function

---
+++ count len.py
@@ -0,0 +1,21 @@
+def string_length(my_string):
+    """returns the length of a string
+    """
+    counter = 0
+    for char in my_string:
+        counter += 1
+    return counter
+
+# taking user input
+string_input = input("enter string : ")
+length = string_length(string_input)
+
+print("length is ", length)
+print(help(string_length))
+
+a_string="abcd 123"
+counter = 0
+for letter in a_string:
+    print(letter)
+    counter += 1
+    print(counter)
