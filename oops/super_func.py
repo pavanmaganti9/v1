d109ce1156c658a69556ffad8a6e39935555d6e9
PVPavanMaganti <pavan@markitome.com>
2019-07-08 08:17:51 +0530

    super function

--- oops/super_func.py
+++ oops/super_func.py
@@ -10,6 +10,8 @@ class Child(Parent2, Parent):
     def __init__(self):
         print('child init')
         super().__init__('Pavan')
+        Parent.__init__(self,'Bindu')
+        Parent2.__init__(self, 'Latika')
 
 child = Child()
 print(Child.__mro__)
